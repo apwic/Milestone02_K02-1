e7ee30332d68c5c457f6cf4c4fcd0d3c3a3a7f7a
Radenz <radenrifqirahman@gmail.com>
2021-08-15 11:34:27 +0700

    feat: Added JoinModal component.

---
+++ src/app/content/join-modal/join-modal.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivityType } from 'src/app/models/activity.model';
+import { ActivityService } from 'src/app/services/activity.service';
+import { ScrimService } from 'src/app/services/scrim.service';
+
+@Component({
+  selector: 'edk-join-modal',
+  templateUrl: './join-modal.component.html',
+  styleUrls: ['./join-modal.component.css'],
+})
+export class JoinModalComponent implements OnInit {
+  title: string;
+
+  constructor(
+    private activityService: ActivityService,
+    private scrimService: ScrimService
+  ) {}
+
+  ngOnInit(): void {
+    // this.title = this.activityService.currentActivity.title;
+  }
+
+  getModalTitle() {
+    let currentActivityRef = this.activityService.currentActivityRef;
+    if (currentActivityRef) {
+      if (currentActivityRef.type === ActivityType.Webinar) {
+        return 'Webinar';
+      } else if (currentActivityRef.type === ActivityType.Course) {
+        return 'Course';
+      } else {
+        return 'Scholarship';
+      }
+    } else {
+      return '';
+    }
+  }
+
+  getTitle() {
+    return this.activityService.currentActivity?.title;
+  }
+
+  isHidden() {
+    return this.activityService.joinModalHidden;
+  }
+
+  close() {
+    this.activityService.hideModal();
+    this.scrimService.hide();
+  }
+
+  join() {
+    this.activityService.join();
+    this.scrimService.hide();
+  }
+}
